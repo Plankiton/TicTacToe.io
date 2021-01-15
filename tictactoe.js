2ba6e3896553b9349bd8992fec8b68e045de9796
Plankiton <pl4nk1ton@gmail.com>
2021-01-15 12:00:36 -0300

    Add the basic interface and responses for clicks

--- tictactoe.js
+++ tictactoe.js
@@ -1,10 +1,26 @@
+function XPlayer() {
+    var img = document.createElement('img');
+    img.setAttribute('class', 'player');
+
+    img.setAttribute('src', 'https://img.icons8.com/ios-filled/50/000000/x.png');
+
+    return img;
+}
+
+function OPlayer() {
+    var img = document.createElement('img');
+    img.setAttribute('class', 'player');
+
+    img.setAttribute('src', 'https://img.icons8.com/ios-filled/50/000000/o.png');
+
+    return img;
+}
+
 class Box {
     value = null;
 
-    play = (p) => {
-        if (['x', 'o'].indexOf(p) > -1) {
-            this.value = p;
-        }
+    play(p) {
+        this.value = p;
     }
 
 }
@@ -12,41 +28,43 @@ class Box {
 class TicTacToe {
     lock = false;
 
-    win = null;
-    boxes = null;
+    round = 0;
 
-    round = 0
+    win = null;
     curr_player = null;
 
-    create = () => {
+    constructor() {
         this.boxes = []
         for (var i = 0; i < 3; i++) {
-            this.boxes.push([])
+            this.boxes.push([]);
             for (var j = 0; j < 3; j++)
                 this.boxes[i].push(new Box());
         }
     }
 
-    lock = () => {
+    lock() {
         this.lock = true;
     }
 
-    unlock = () => {
+    unlock() {
         this.lock = false;
     }
 
     isLock = () => this.lock;
 
-    play = (x, y) => {
+    play(x, y) {
         if (!this.winner && !this.boxes[x-1][y-1].value) {
             this.curr_player = this.round%2? "x" : "o";
-            this.boxes[x-1, y-1].play(this.curr_player)
+            console.log(this.curr_player, "playing on ", x, ",", y);
+            this.boxes[x-1][y-1].play(this.curr_player);
 
             this.round ++
+        } else {
+            console.log(this.curr_player, "did invalid play on ", x, ",", y);
         }
     }
 
-    getWin = () => {
+    getWin() {
         var d = 0;
         var di = 0;
         var t = 0;
@@ -90,4 +108,46 @@ class TicTacToe {
             this.winner = "n";
     }
 
+}
+
+class Board {
+    constructor(game, element, subbox=false) {
+        this.game = game;
+        this.board = element;
+        this.board.innerHTML = "";
+
+        for (var i = 1; i <= 3; i++) {
+            var row = document.createElement("div");
+            row.setAttribute("class", "row");
+            this.board.appendChild(row);
+
+            for (var j = 1; j <= 3; j++) {
+                var box = document.createElement("div");
+                box.setAttribute("class", "box" + (subbox?" subbox":""));
+
+                box.id = this.getBoxId(i, j).slice(1,4);
+                box.addEventListener("click", (e) => {
+                    var curr_box = e.path[0];
+
+                    var x = Number(curr_box.id[1]);
+                    var y = Number(curr_box.id[2]);
+
+                    this.play(x, y);
+                });
+
+                row.appendChild(box);
+            }
+        }
+    }
+
+    getBoxId = (x, y) => `#b${x}${y}`
+
+    play(x, y) {
+
+        this.game.play(x, y);
+        var curr_box = this.board.querySelector(this.getBoxId(x, y));
+        if (curr_box.innerHTML == "")
+            curr_box.appendChild(this.game.curr_player=='x'?XPlayer():OPlayer());
+
+    }
 }
